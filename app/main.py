ea655af159d629c30808f04235ac1d97489a8a9a
sanggubot <sanggubot@gmail.com>
2023-10-13 09:00:31 +0000

    refactor:move input and output to model

--- app/main.py
+++ app/main.py
@@ -1,51 +1,26 @@
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel, Field
+from main_model import *
+
 
 app = FastAPI(
     title="project-baselines/python-devcontainer-fastapi",
     version="0.1.0",
+    responses={
+        status.HTTP_404_NOT_FOUND: {"description": "The item was not found"},
+        status.HTTP_422_UNPROCESSABLE_ENTITY: {"description": "Invalid ID supplied"},
+        status.HTTP_200_OK: {"description": "Item requested by ID"},
+    },
 )
-
-responses = {
-    status.HTTP_404_NOT_FOUND: {"description": "The item was not found"},
-    status.HTTP_422_UNPROCESSABLE_ENTITY: {"description": "Invalid ID supplied"},
-    status.HTTP_200_OK: {"description": "Item requested by ID"},
-}
-
-
-class RequestModel(BaseModel):
-    id: str = Field(title="id of item", description="id of item")
-
-    class Config:
-        json_schema_extra = {
-            "examples": [
-                {"id": "foo"},
-                {"id": "bar"},
-            ]
-        }
-
-
-class ResponseModel(BaseModel):
-    value: str = Field(title="value of item", description="value of item")
-
-    class Config:
-        json_schema_extra = {
-            "examples": [
-                {"value": "foofoo"},
-                {"value": "barbar"},
-            ]
-        }
 
 
 @app.post(
     path="/items",
     tags=["items"],
     description="Get item by ID",
-    response_model=ResponseModel,
-    responses=responses,
+    response_model=ItemsResponseModel,
 )
-async def read_item(body: RequestModel) -> ResponseModel:
+async def read_item(body: ItemsRequestModel) -> ItemsResponseModel:
     if body.id == "foo":
         return {"value": "bar"}
     return JSONResponse(
